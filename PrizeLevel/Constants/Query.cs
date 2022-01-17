52e139c26737f225f9d9b4966e4e9bfe3b6cd1f1
anhnth24 <75232590+anhnth24@users.noreply.github.com>
2022-01-17 21:51:44 +0700

    new const

--- PrizeLevel/Constants/Query.cs
+++ PrizeLevel/Constants/Query.cs
@@ -10,5 +10,7 @@ namespace PrizeLevel.Constants
     {
         public const string getSelections645 = "SELECT COMBINATION,QUANTITY FROM CORE_DCS_645 where CAST('20'|| RIGHT(FILENAME,6) AS DATE) = ";
         public const string getBingo645 = "SELECT TOP 1 NUMBER FROM CORE_DL WHERE LIATYPE =1 AND DRAWDATE = ";
+        public const string getSelections655 = "";
+
     }
 }
